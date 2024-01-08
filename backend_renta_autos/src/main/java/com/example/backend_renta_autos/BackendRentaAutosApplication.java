0f37b79fc6df8245c14e5b4f40d4428ef3b9021f
Jonathan-Te <jonathantbtwd@gmail.com>
2024-01-07 21:58:28 -0500

    creacion backend

---
+++ backend_renta_autos/src/main/java/com/example/backend_renta_autos/BackendRentaAutosApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_renta_autos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendRentaAutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendRentaAutosApplication.class, args);
+	}
+
+}
---
+++ backend_renta_autos/src/test/java/com/example/backend_renta_autos/BackendRentaAutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_renta_autos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendRentaAutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
