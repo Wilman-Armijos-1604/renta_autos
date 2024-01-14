11378e3460470d5092a475affcc6647a426f28de
Jonathan-Te <jonathantbtwd@gmail.com>
2024-01-14 18:02:00 -0500

    creacion de archivos para pantalla login

--- frontend_renta_autos/src/app/app.routes.ts
+++ frontend_renta_autos/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/screens/login/login.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path: "**",redirectTo: 'login'},
+    {path: 'login', component:LoginComponent}
+   
+];
